fdb07a02601d12d1b715b49392dbd42ad3c21403
rashedevs <rasheduap2015@gmail.com>
2022-07-18 21:17:46 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./Pages/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>Hello Fake Shop</h1>
     </div>
   );
---
+++ src/Pages/Navbar/Navbar.js
@@ -0,0 +1,87 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark px-5">
+      <div className="container-fluid text-white">
+        <a className="navbar-brand fw-bold text-white" href="/">
+          Fake Shop
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link active fw-bolder text-white"
+                aria-current="page"
+                href="/"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link fw-bolder text-white" href="/">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle fw-bolder text-white"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
